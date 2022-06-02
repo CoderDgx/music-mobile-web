1a03a7b3a23729c0443b480907cb84413df65634
CoderDgx <1181571719@qq.com>
2022-06-02 11:36:10 +0800

    ci: 修改项目配置

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const CracoLessPlugin = require('craco-less')
 
 const pathResolve = (pathUrl) => path.join(__dirname, pathUrl)
 
@@ -10,18 +9,5 @@ module.exports = {
       '@assets': pathResolve('src/assets'),
       '@components': pathResolve('src/components')
     }
-  },
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            modifyVars: { '@primary-color': '#1DA57A' },
-            javascriptEnabled: true
-          }
-        }
-      }
-    }
-  ]
+  }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '@/App.less'
+import './App.scss'
 
 function App() {
   return <div className="App">Hello World!</div>
